6ceb18f9c452eb1bfea1c92691895d36218660f3
lvitters <luvit023@gmail.com>
2026-02-05 15:30:04 +0100

    remove all the text glow

--- src/lib/components/yearSelect.svelte
+++ src/lib/components/yearSelect.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <!-- horizontal list of year buttons for filtering content -->
-<div class="text-glow-white flex w-full flex-wrap items-center gap-4 text-[clamp(1rem,3vw,1.5rem)] leading-none">
+<div class="flex w-full flex-wrap items-center gap-4 text-[clamp(1rem,3vw,1.5rem)] leading-none">
 	{#each years || [] as year}
 		<button
 			class="cursor-pointer py-1 whitespace-nowrap hover:bg-[var(--text-color)] hover:text-[var(--bg-color)] {year ===
